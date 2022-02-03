b0e648873922ff525d8804961d3b74927feb0733
Yusuf <yusuf.khan@berkeley.edu>
2022-02-03 01:41:56 -0800

    added tests for adopt method, retrieval of single pets owner, and retrieval of all pet owners

--- test/TestAdoption.sol
+++ test/TestAdoption.sol
@@ -10,13 +10,26 @@ contract TestAdoption {
 
   // The id of the pet that will be used for testing
   uint expectedPetId = 8;
+  //testing adopt function
   function testUserCanAdoptPet() public {
       uint returnedId = adoption.adopt(expectedPetId);
-
       Assert.equal(returnedId, expectedPetId, "Adoption of the expected pet should match what is returned.");
   }
 
   // The expected owner of adopted pet is this contract
   address expectedAdopter = address(this);
+  //testing single pet owner retrieval
+  function testGetAdopterAddressByPetId() public {
+    address returnedAdopter = adoption.adopters(expectedPetId);
+    Assert.equal(returnedAdopter, expectedAdopter, "Owner of the expected pet should be this contract ");
+  }
+
+  //testing retrieval of all pet owners
+  function testGetAdopterAddressByPetIdInArray() public {
+    address[16] memory returnedAdopters = adoption.getAdopters();
+
+    Assert.equal(returnedAdopters[expectedPetId], expectedAdopter, "Owner of the expected pet should be this contract" );
+  }
+
 
 }
